d4059a20d1c947a4e30246ad1ea385ed71ff6154
unknown <prixlin@gmail.com>
2021-01-14 15:05:10 -0300

    corrigindo erros

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let canvas = document.getElementById=("snake");
+let canvas = document.getElementById("snake");
 let context = canvas.getContext("2d");
 let box = 32;
 let snake = [];
@@ -8,27 +8,28 @@ snake[0] = {
     y: 8 * box
 
 }
-let direction ="right";
+
+let direction = "right";
 let food = {
     x:Math.floor(Math.random() * 15 + 1) * box,
-    y:Math.floor(Math.random() * 15 +1) * box
+    y:Math.floor(Math.random() * 15 + 1) * box
 }
 
 function criarBG() {
-    context.fillStyle = "lightpurple";
-    context.fillRect(0, 0, 16 * box, 16 * box);
+    context.fillStyle = "lightgreen";
+    context.fillRect(0, 0, 16*box, 16*box);
 
 }
 
 function criarCobrinha(){
-    for(i=0; i < snake.leght; i++){
-        context.fillStyle = "green"
+    for(i = 0; i < snake.length; i++){
+        context.fillStyle = "purple";
         context.fillRect(snake[i].x, snake[i].y, box, box);
 
     }
 }
 
-function drawfood() {
+function drawFood(){
     context.fillStyle = "red";
     context.fillRect(food.x, food.y, box, box);
 }
@@ -42,15 +43,15 @@ function update(event) {
     if(event.keyCode == 40 && direction != "up") direction = "down";
 }
 
-function iniarJogo(){
-    if(snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
+function iniciarJogo(){
+    if(snake[0].x > 15*box && direction == "right") snake[0].x = 0;
     if (snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
-    if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
+    if(snake[0].y > 15*box && direction == "down") snake[0].y = 0;
     if (snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
     
     for(i = 1; i < snake.length; i++){
         if(snake[0].x == snake[i].x && snake[0].y == snake[i].y){
-          clearInterval(jogo);
+            clearInterval(jogo);
             alert('Game Over :(');
         }
         
@@ -58,6 +59,7 @@ function iniarJogo(){
 
     criarBG();
     criarCobrinha();
+    drawFood();
 
     let snakeX = snake[0].x;
     let snakeY = snake[0].y;
